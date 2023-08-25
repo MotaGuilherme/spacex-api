ec2a4d4c17c7560e79a79ae94e9a81e89223681b
MotaGuilherme <metatronmota@gmail.com>
2023-08-24 22:00:43 -0300

    feat: calling swagger in the file server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import swaggerUI from 'swagger-ui-express'
 
 import router from './src/routes/index.js';
+import swaggerSpecs from './src/config/swaggerConfig.js'
+
 
 const app = express()
 
@@ -11,6 +14,9 @@ app.use(express.json())
 app.use(router)
 
 mongoose.connect('mongodb://127.0.0.1:27017/spacex');
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
+
 
 app.listen(3003, () => {
     console.log("App Iniciando na porta 3003")
